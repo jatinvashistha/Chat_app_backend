c124ed2264caf5ce087fd85f34989cfdc674bdf3
Jatin Vashisth <136696926+jatinvashistha@users.noreply.github.com>
2024-03-22 17:11:37 +0000

    done

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chat-app-jatin-vashisth.netlify.app",
+    origin: "https://v8d685-3000.csb.app",
   },
 });
 
