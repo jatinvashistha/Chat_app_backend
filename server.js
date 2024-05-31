1459acd5ab2b7f8ac0bafdc2a0bcd7166390ed54
Jatin Vashisth <136696926+jatinvashistha@users.noreply.github.com>
2024-05-31 04:56:39 +0000

    done

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://87g4j8-3000.csb.app",
+    origin: "https://chat-app-git-main-jatinvashisthas-projects.vercel.app",
   },
 });
 
